ca910c8f8b9aac7edd4c318f210dda14abfba784
michal92299 <chudybyczek2137@gmail.com>
2026-01-28 20:24:49 +0100

    Update build.rs

--- source-code/build.rs
+++ source-code/build.rs
@@ -1,11 +1,11 @@
 fn main() {
-    cxx_build::bridge("src/main.rs")
-        .file("src/apt_bridge.cpp")
-        .flag_if_supported("-std=c++17")
-        .compile("legendary-apt");
+    cxx_build::bridge("src/ffi.rs")
+    .file("src/apt_bridge.cpp")
+    .flag_if_supported("-std=c++17")
+    .compile("legendary-apt");
 
     println!("cargo:rustc-link-lib=apt-pkg");
-    println!("cargo:rerun-if-changed=src/main.rs");
+    println!("cargo:rerun-if-changed=src/ffi.rs");
     println!("cargo:rerun-if-changed=src/apt_bridge.cpp");
     println!("cargo:rerun-if-changed=src/apt_bridge.h");
 }
